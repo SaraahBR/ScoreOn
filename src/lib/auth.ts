6dbc105fec8941f345da95399cec8d716f5dfd4f
Sarah Hernandes <vihernandesbr@gmail.com>
2025-08-12 23:59:50 -0300

    fix: Autentição e rota da página Login.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,11 +2,16 @@ import type { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
+
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
   ],
+
   session: { strategy: "jwt" },
+
+  pages: { signIn: "/login" },
 };
