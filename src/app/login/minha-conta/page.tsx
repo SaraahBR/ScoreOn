93fa86bd4eaec3bb28d18546d13c2440e0046551
Sarah Hernandes <vihernandesbr@gmail.com>
2025-08-13 02:01:30 -0300

    feat: Sessão Sexo em DropDown e somente um endereço.

--- src/app/login/minha-conta/page.tsx
+++ src/app/login/minha-conta/page.tsx
@@ -1,7 +1,19 @@
 "use client";
 
 import { useSession } from "next-auth/react";
-import { Box, Button, Container, Grid, Paper, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  Paper,
+  TextField,
+  Typography,
+  Select,
+  MenuItem,
+  InputLabel,
+  FormControl,
+} from "@mui/material";
 
 export default function MinhaConta() {
   const { data: session } = useSession();
@@ -36,12 +48,16 @@ export default function MinhaConta() {
           placeholder="Digite seu CPF"
           margin="normal"
         />
-        <TextField
-          label="Sexo"
-          fullWidth
-          placeholder="Informe seu sexo"
-          margin="normal"
-        />
+
+        <FormControl fullWidth margin="normal">
+          <InputLabel>Sexo</InputLabel>
+          <Select defaultValue="">
+            <MenuItem value="Feminino">Feminino</MenuItem>
+            <MenuItem value="Masculino">Masculino</MenuItem>
+            <MenuItem value="Não Informar">Não Informar</MenuItem>
+          </Select>
+        </FormControl>
+
         <TextField
           label="Data de nascimento"
           type="date"
@@ -60,56 +76,13 @@ export default function MinhaConta() {
         </Box>
       </Paper>
 
-      {/* Endereços */}
+      {/* Endereço */}
       <Typography variant="h6" sx={{ mb: 2 }}>
-        Endereços
+        Endereço
       </Typography>
-
       <Grid container spacing={3}>
-        {/* Endereço de cobrança */}
-        <Grid item xs={12} md={6}>
+        <Grid item xs={12}>
           <Paper elevation={1} sx={{ p: 3 }}>
-            <Typography variant="subtitle1" sx={{ mb: 2 }}>
-              Endereço de cobrança padrão
-            </Typography>
-            <TextField
-              label="Endereço"
-              fullWidth
-              placeholder="Digite seu endereço"
-              margin="normal"
-            />
-            <TextField
-              label="Cidade"
-              fullWidth
-              placeholder="Digite sua cidade"
-              margin="normal"
-            />
-            <TextField
-              label="CEP"
-              fullWidth
-              placeholder="Digite seu CEP"
-              margin="normal"
-            />
-            <TextField
-              label="Telefone"
-              fullWidth
-              placeholder="Digite seu telefone"
-              margin="normal"
-            />
-            <Box sx={{ mt: 2 }}>
-              <Button variant="outlined" color="primary">
-                Salvar endereço
-              </Button>
-            </Box>
-          </Paper>
-        </Grid>
-
-        {/* Endereço de entrega */}
-        <Grid item xs={12} md={6}>
-          <Paper elevation={1} sx={{ p: 3 }}>
-            <Typography variant="subtitle1" sx={{ mb: 2 }}>
-              Endereço de entrega padrão
-            </Typography>
             <TextField
               label="Endereço"
               fullWidth
